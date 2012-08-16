0e9e9f329a60f209df240f39774787056fe7ede9
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-08-16 16:56:20 -0500

    refactor ValidationMatcherTest

--- test/cases/validation_test.rb
+++ test/cases/validation_test.rb
@@ -2,22 +2,29 @@ require 'test_helper'
 
 class ValidationMatcherTest < MiniTest::Unit::TestCase
   def setup
-    @mock = MiniTest::Mock.new
-    @mock.expect :is_a?, true, [Class]
-    @validator = ValidationMatcher.new :field, :type
+    @validator ||= ValidationMatcher.new :field, :type
   end
 
   test 'returns true if validation type exists' do
-    type = MiniTest::Mock.new
-    type.expect :kind, :type
-    @mock.expect :validators_on, [type], [:field]
+    subject = create_subject :field, :type
 
-    assert @validator.matches? @mock
+    assert @validator.matches? subject
   end
 
   test 'returns false if validation type not exists' do
-    @mock.expect :validators_on, [], [:field]
+    subject = create_subject :field, :another_type
 
-    assert !@validator.matches?(@mock)
+    assert !@validator.matches?(subject)
+  end
+
+  private
+
+  def create_subject field, type
+    arg = MiniTest::Mock.new
+    arg.expect :kind, type
+
+    subject = MiniTest::Mock.new
+    subject.expect :is_a?, true, [Class]
+    subject.expect :validators_on, [arg], [field]
   end
 end
