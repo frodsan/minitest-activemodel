145d21c9f28b06cc07e6150b7366bd7eac60dd1e
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-08-27 10:02:30 -0500

    mv matchers/ minitest-activemodel/matchers/

--- lib/minitest-activemodel.rb
+++ lib/minitest-activemodel.rb
@@ -1,6 +1,6 @@
 require 'minitest-activemodel/version'
 
 require 'minitest/matchers'
-require 'matchers/validation.rb'
-require 'matchers/activemodel/validate_presence_matcher'
-require 'matchers/activemodel/validate_acceptance_matcher'
+require 'minitest-activemodel/matchers/validation.rb'
+require 'minitest-activemodel/matchers/activemodel/validate_presence_matcher'
+require 'minitest-activemodel/matchers/activemodel/validate_acceptance_matcher'
