d00c77f655b0ce43302bf18c583b6a33ed50d978
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-08-26 21:40:54 -0500

    add failing test with negative message

--- test/cases/activemodel/validate_acceptance_matcher_test.rb
+++ test/cases/activemodel/validate_acceptance_matcher_test.rb
@@ -9,7 +9,11 @@ class ValidateAcceptanceTest < MiniTest::Unit::TestCase
     assert_wont validate_acceptance_of(:not_accepted), User
   end
 
-  test 'must validate acceptance of an attribute with a given accepted value' do
+  test 'must validate acceptance of an attribute with an accepted value' do
     assert_must validate_acceptance_of(:terms_of_service).accept_with(true), User
+  end
+
+  test 'must not validate acceptance of an attribute with an unaccepted value' do
+    assert_wont validate_acceptance_of(:terms_of_service).accept_with('1'), User
   end
 end
